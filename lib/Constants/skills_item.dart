214996cf3465284db43b6600b58b28baf1b2331f
Kalash <kalashkose0@gmail.com>
2025-04-20 00:56:48 +0530

    ,.,

---
+++ lib/Constants/skills_item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+const List<Map> platformItems = [
+  {
+    "img": "assets/images/AA_logo.png",
+    "title": "Android Dev",
+  },
+  {
+    "img": "assets/images/www.png",
+    "title": "Web Dev",
+  },
+  {
+    "img": "assets/images/icons8-apple-logo-50.png",
+    "title": "IOS Dev",
+  },
+  // {
+  //   "img": "assets/images/AA_logo.png",
+  //   "title": "Android Dev",
+  // },
+];
+
+const List<Map> skillItem = [
+  {
+    "img": "assets/images/Flutter Icon.png",
+    "title": "IOS Dev",
+  },
+  {
+    "img": "assets/images/icons8-apple-logo-50.png",
+    "title": "IOS Dev",
+  },
+  {
+    "img": "assets/images/icons8-apple-logo-50.png",
+    "title": "IOS Dev",
+  },
+  {
+    "img": "assets/images/icons8-apple-logo-50.png",
+    "title": "IOS Dev",
+  },
+  {
+    "img": "assets/images/icons8-apple-logo-50.png",
+    "title": "IOS Dev",
+  },
+  {
+    "img": "assets/images/icons8-apple-logo-50.png",
+    "title": "IOS Dev",
+  },
+  {
+    "img": "assets/images/icons8-apple-logo-50.png",
+    "title": "IOS Dev",
+  },
+  {
+    "img": "assets/images/icons8-apple-logo-50.png",
+    "title": "IOS Dev",
+  },
+];
